f4b9f36239c27beffd2ca7978eb3345a9f3a56a4
Zach Johnson <zgjohnson94@gmail.com>
2023-01-29 09:18:26 -0600

    Initial Commit

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by Zach Johnson on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
